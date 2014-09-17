7f20100f8a9f15bff372a1a8da05f2c815961749
Chen-Huanyi <Huanyi-chen@foxmail.com>
2014-09-18 01:08:48 +0800

    finish all functions, need testing

--- foxmailer.js
+++ foxmailer.js
@@ -1,27 +1,57 @@
 var nodemailer = require("nodemailer")
+var XLSX = require("xlsx");
 
+/* *
+ * this module need Excel component if running
+ */
+if(typeof require !== 'undefined') XLSX = require('xlsx');
+var workbook = XLSX.readFile('receivers.xlsx');
+
+var worksheet;
+var sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(function(y) {
+	worksheet = workbook.Sheets[y];
+});
+console.log("Read excel success, start send email");
+
+/* *
+ * mail function
+ */
 var smtpTransport = nodemailer.createTransport("SMTP",{
 	host:"smtp.hi-design.hk",
 	secureConnection: true,
-	port:
+	port: 995,
 	auth:{
 		user:"chloe@hi-design.hk",
 		pass:""
 	}
 });
 
-var mailOptions = {
-	from:"chloe@hi-design.hk",
-	to:"",
-	subject:"hello nodemailer",//mail title
-	html:"<b>thanks for visiting</b> Hi, nice to meet you!"//html content
-}
+for (z in worksheet) {
+	/* use slow method to find all email address*/
+	switch(z[0])
+	{
+	case '!':
+		continue;
+		break;
+	case 'A':
+		var mailOptions = {
+			from:"chloe@hi-design.hk",
+			to:JSON.stringify(worksheet[z].v),
+			subject:"hello nodemailer",//mail title
+			html:"<b>thanks for visiting</b> Hi, you are" + JSON.stringify(worksheet[z].v) + "!",//html content
+		}
 
-smtpTransport.sendMail(mailOptions, function(error, response){
-	if(error){
-		console.log(error);
-	} else {
-		console.log("Message sent:" + response.message);
+		smtpTransport.sendMail(mailOptions, function(error, response){
+			if(error){
+				console.log(error);
+			} else {
+				console.log("Message sent:" + response.message);
+			}
+			//smtpTransport.close();
+		})
+		break;
+	default:
+		break;
 	}
-	//smtpTransport.close();
-})
+}
